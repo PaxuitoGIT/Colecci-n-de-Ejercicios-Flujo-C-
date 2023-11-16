e319237862a32084a15f52612754009b7b50a096
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2023-11-16 11:46:19 +0000

    commit

--- main_mates.cpp
+++ main_mates.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 
 namespace Matematicas {
-    int suma (int a, b){
+    int suma (int a, int b){
     return a + b;
     }
     int resta (int a, int b) {
@@ -15,12 +15,10 @@ int main() {
     std::string imput1;
     std::getline(std::cin, imput1);
     std::stringstream(imput1) >> a;
-    stream1 >> num1;
     std::cout << "Ingresa otro número: ";
     std::string imput2;
     std::getline(std::cin, imput2);
     std::stringstream(imput2) >> b;
-    stream2 >> num2;
     int resultadosuma = Matematicas::suma(a, b);
     int resultadoResta = Matematicas::resta(a, b);
     std::cout << "El resultado de la suma es: " << resultadosuma << "\n";
