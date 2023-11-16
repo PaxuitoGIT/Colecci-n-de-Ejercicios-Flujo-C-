7d289713c968530aebaad38162098cb0bf72ee72
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2023-11-16 12:12:17 +0000

    commit

--- main_ciencia.cpp
+++ main_ciencia.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
-#include <iomanip>
-#include <sstream>
+#include <string>
+#include "triangulo.cpp"  // Incluir el código de triangulo.cpp
+#include "circulo.cpp"    // Incluir el código de circulo.cpp
 
 int main() {
-    double num;
-    std::string line;
-    std::cout << "Ingresa un número: ";
-    std::getline (std::cin, line);
-    std::stringstream(line) >> num;
+    double base, altura, radio;
 
-    std::cout << "El número ingresado es: " << std::fixed << std::setprecision(2) << num << std::endl;
-    
+    // Solicitar al usuario que ingrese los datos para el triángulo
+    std::cout << "Ingresa la base del triángulo: ";
+    std::cin >> base;
+    std::cout << "Ingresa la altura del triángulo: ";
+    std::cin >> altura;
+
+    // Utilizar la función del espacio de nombres Geometria::Triangulo
+    double areaTriangulo = Geometria::Triangulo::calcularArea(base, altura);
+    std::cout << "El área del triángulo es: " << areaTriangulo << std::endl;
+
+    // Solicitar al usuario que ingrese los datos para el círculo
+    std::cout << "Ingresa el radio del círculo: ";
+    std::cin >> radio;
+
+    // Utilizar la función del espacio de nombres Geometria::Circulo
+    double areaCirculo = Geometria::Circulo::calcularArea(radio);
+    std::cout << "El área del círculo es: " << areaCirculo << std::endl;
+
     return 0;
 }
--- triangulo.cpp
+++ triangulo.cpp
@@ -1,20 +1,8 @@
 #include <iostream>
-#include <sstream 
-#include <cmath>
+#include <sstream>
 
 namespace Geometria {
-    double calcularArea () {
-        double base, altura;
-        std::string line;
-        
-        std::cout << "Ingresa la base: ";
-        std::getline(std::cin, line);
-        std::stringstream(line) >> base;
-
-        std::cout << "Ingresa la altura: ";
-        std::getline(std::cin, line);
-        std::stringstream(line) >> altura;
-
+    double calcularAreaTriangulo(double base, double altura) {
         return 0.5 * base * altura;
-        })
+    }
 }
