d3f3bb4ea2f66498c7ac136f5540142640203268
JPabloLobato <146821982+JPabloLobato@users.noreply.github.com>
2023-11-16 11:26:05 +0000

    2commit

---
+++ main_mates.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int num;
+    std::cout << "Ingresa un número: ";
+    std::cin >> num;
+    std::cout << "El número ingresado es: " << num << std::endl;
+    return 0;
+}
